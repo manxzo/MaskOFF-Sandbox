7e8fd41202a7128fc05d184bf4bb4f1950869203
manxzo <manzoball@gmail.com>
2025-02-21 16:36:55 +0800

    added navigation for "/"

--- MaskOFF-Client/src/App.tsx
+++ MaskOFF-Client/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes, useParams, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 
 // Import your pages (ensure you create/update these pages using @heroui/react components)
@@ -10,6 +10,7 @@ const App = () => {
   //For authenticated pages use <Route path={`/${username}/path`} element = {<Element/>}/>
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );
